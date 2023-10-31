6fbf89998abeb5ff80876d4bbf0ace11823ea90f
mana0001 <robin0515.m@gmail.com>
2023-10-31 17:35:57 +0900

    投稿処理用モデル

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,20 +2,19 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Posts</title>
-        
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        
     </head>
-    <body class="antialiased">
+    <body>
         <h1 class="title">
             {{ $post->title }}
         </h1>
         <div class="content">
             <div class="content__post">
                 <h3>本文</h3>
-                <p class='body'>{{ $post->body }}</p>    
+                <p>{{ $post->body }}</p>    
             </div>
         </div>
         <div class="footer">
